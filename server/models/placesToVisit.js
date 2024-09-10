5b4cbe185f4a6e02964006300f133a03e3518630
labbaci <labbaci@student.chalmers.se>
2024-09-10 19:19:52 +0200

    #2 Add placeToVisitSchema var and mongoose

---
+++ server/models/placesToVisit.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var placesToVisitSchema = new Schema({
+    address: { type: String, required: true, unique: true },
+    rating: { type: Number, required: false },
+    content: { type: String, required: true },
+    tags: { type: Array, required: true },
+  });
+  
+  // Export the User model
+  module.exports = mongoose.model('placesToVisit', placesToVisitSchema);
+
+  
