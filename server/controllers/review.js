f2550966c95b1c839fc769792c9676494e0a649b
manely <manely@student.chalmers.se>
2024-09-12 14:20:56 +0200

    #3 delete all reviews function

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -16,3 +16,26 @@ router.get("/", async (req, res) => {
     const reviews = await Review.find();
     res.send(reviews);
 });
+
+router.delete('/', async (req, res) => {
+    try {
+        const review = await Review.deleteMany();  // Removes all reviews
+
+        if (review.deletedCount === 0) {
+            return res.status(404).json({ "message": "There are no reviews to delete" });
+        }
+
+        return res.status(200).json({ "message": "All reviews deleted successfully" });
+
+    } catch (err) {
+        res.status(500).json({ "message": "An error occurred while deleting reviews" });
+    }
+});
+
+
+
+
+
+
+
+
