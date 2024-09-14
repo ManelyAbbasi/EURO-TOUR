01c7522bb8a733c7150ed910ce370990db5effcb
manely <manely@student.chalmers.se>
2024-09-14 11:40:04 +0200

    #3 removed delete function in placesToVisit

--- server/controllers/placesToVisit.js
+++ server/controllers/placesToVisit.js
@@ -18,17 +18,3 @@ router.post("/", async (req, res) => {
     res.send(result);
 });
 
-router.delete('/', async (req, res) => {
-    try {
-        const result = await PlacesToVisit.deleteMany();  // Removes all placesToVisit
-
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ "message": "There are no places to delete" });
-        }
-
-        return res.status(200).json({ "message": "All places deleted successfully" });
-
-    } catch (err) {
-        res.status(500).json({ "message": "An error occurred while deleting places" });
-    }
-});
