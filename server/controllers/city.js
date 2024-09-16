94aa8f17a356ef31189e64ca084263885290b614
labbaci <labbaci@student.chalmers.se>
2024-09-16 15:37:36 +0200

    #4 Create put/:postcode method for city controller

--- server/controllers/city.js
+++ server/controllers/city.js
@@ -9,6 +9,7 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
     const city = new City({
+        postcode: req.body.postcode,
         cityName: req.body.cityName,
         country: req.body.country,
         statistics: req.body.statistics,
@@ -22,4 +23,19 @@ router.post("/", async (req, res) => {
 router.get("/:postcode", async (req, res) =>{
     const city = await City.find({ postcode: req.params.postcode });
     res.send(city);
+})
+
+router.put("/:postcode", async (req, res) =>{
+    const postcode = req.params.postcode;
+    const updatedCity = {
+        "postcode": postcode,
+        "cityName": req.body.cityName,
+        "country": req.body.country,
+        "statistics": req.body.statistics,
+        "facts": req.body.facts,
+        "tags": req.body.tags,
+    }
+    const cities = await City.find();
+    cities[postcode] = updatedCity;
+    res.send(updatedCity);
 })
