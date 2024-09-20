e401a57760c6d9b3f695dfe9d865fbec4cd4e024
manely <manely@student.chalmers.se>
2024-09-20 17:31:55 +0200

    #11 usersController issues fixed

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,46 +3,6 @@ const express = require("express");
 const router = express.Router();
 //const usersModel = require("../models/usersModel");
 
-function validateUserData(data) {
-    const errors = [];
-
-    if (!data.username || typeof data.username !== 'string') {
-        errors.push('Username is required and must be a string.');
-    }
-
-    if (!data.password || typeof data.password !== 'string') {
-        errors.push('Password is required and must be a string.');
-    }
-
-    if (!data.birthDate || isNaN(new Date(data.birthDate))) {
-        errors.push('Valid birth date is required.');
-    } else {
-        const minDate = new Date(1920, 1, 1);
-        const maxDate = new Date(2012, 1, 1);
-        const userBirthDate = new Date(data.birthDate);
-        
-        if (userBirthDate < minDate || userBirthDate > maxDate) {
-            errors.push('Birth date must be between 1920 and 2012.');
-        }
-    }
-
-    if (data.isLGBTQIA !== undefined && typeof data.isLGBTQIA !== 'boolean') {
-        errors.push('isLGBTQIA must be a boolean value.');
-    }
-
-    if (data.gender && typeof data.gender !== 'string') {
-        errors.push('Gender must be a string.');
-    } else if (data.gender && !['male', 'female', 'non-binary', 'other'].includes(data.gender)) {
-        errors.push('Gender must be one of the following: male, female, non-binary, other.');
-    }
-
-    if (data.isAdmin !== undefined && typeof data.isAdmin !== 'boolean') {
-        errors.push('isAdmin must be a boolean value.');
-    }
-
-    return errors; 
-}
-
 async function getAllUsers(req, res) {
     try {
         const users = await UsersModel.find(); // Fetch users from the database
@@ -57,22 +17,14 @@ async function getAllUsers(req, res) {
 
 
 async function createUser(req, res, next) {
-    const errors = validateUserData(req.body);
-    
-    if (errors.length > 0) {
-        return res.status(404).send({ errors }); 
-    }
-
-    const { username } = req.body;
     try {
-        const existingUser = await UsersModelModel.findOne({ username });
+        const existingUser = await UsersModel.findOne({ postcode: req.body.postcode });
 
         if (existingUser) {
             return res.status(400).send({ message: 'User with this username already exists' });
         }
 
         const users = new UsersModel(req.body);
-
         await users.save();
         res.status(201).send(users);
     } catch (err) {
@@ -122,8 +74,7 @@ async function patchUser(req, res, next) {
             return res.status(404).send({ "message": "User not found" });
         }
 
-        user.password = req.body.password !== undefined ? req.body.password : user.password;
-
+        user.password = req.body.password || user.password;
         await user.save();
         res.status(200).send(user); 
     } catch (err) {
