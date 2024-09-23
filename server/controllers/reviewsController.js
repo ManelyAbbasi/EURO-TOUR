a7ed0fb0d6eb167cf6721889924675d50c956bea
manely <manely@student.chalmers.se>
2024-09-23 17:31:46 +0200

    #12 Refactor reviewsController

--- server/controllers/reviewsController.js
+++ server/controllers/reviewsController.js
@@ -4,18 +4,15 @@ const router = express.Router();
 const UsersModel = require("../models/usersModel");
 
 
-
-
 async function createReview(req, res) {
+
     try {
-        // Find the user by their username from the request body
         const user = await UsersModel.findOne({ username: req.body.user });
-        console.log(user); // Add this line
+        console.log(user); 
         
         if (!user) {
             return res.status(404).send({ error: "User not found" });
         }
-
         if (typeof req.body.rating !== 'number') {
             return res.status(400).send({ "message": "Invalid rating: must be a non-empty number" });
         }
@@ -26,30 +23,25 @@ async function createReview(req, res) {
             return res.status(400).send({ "message": "Invalid content: must be a non-empty string" });
         }
 
-        // Create the review
         const review = new ReviewsModel({
             rating: req.body.rating,
             content: req.body.content,
             date: Date.now(),
-            user: user._id, // Set user field to the found user's _id
+            user: user._id, 
         });
-        console.log(review); // Add this line
         
-        // Save the review to the database  
         await review.save();
-        
-        // Send the created review as the response
         res.status(201).send(review);
     } catch (err) {
-        // Log the error for better debugging
         console.error("Error creating review:", err);
         res.status(500).send({ message: "An error occurred while creating the review", error: err.message });
     }
 }        
 
+
 async function getAllReviews(req, res) {
     try {
-        const reviews = await ReviewsModel.find(); // Fetch users from the database
+        const reviews = await ReviewsModel.find(); 
         if (!reviews || reviews.length === 0) {
             return res.status(404).send({ error: 'No reviews found.' });
         }
@@ -59,13 +51,13 @@ async function getAllReviews(req, res) {
     }
 }
 
+
 async function deleteOldReviews(req, res) { 
+
     try {
-        // Calculate the date 5 years ago
         const fiveYearsAgo = new Date();
         fiveYearsAgo.setFullYear(fiveYearsAgo.getFullYear() - 5);
 
-        // Delete reviews older than 5 years
         const result = await ReviewsModel.deleteMany({ date: { $lt: fiveYearsAgo } });
 
         if (result.deletedCount === 0) {
@@ -77,7 +69,6 @@ async function deleteOldReviews(req, res) {
         res.status(500).send({ "message": "An error occurred while deleting reviews" });
     }
 }
-
 
 module.exports = {
     createReview, 
