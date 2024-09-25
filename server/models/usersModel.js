0e91fcfb498ac3f89a9f5b659cc6201e6f937255
renaa <renaa@chalmers.se>
2024-09-25 13:15:07 +0200

    #8 Fix date format on birthdate, and add first test

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -10,14 +10,21 @@ var usersSchema = new Schema({
     password: { type: String, required: true },
     birthDate: { type: Date, required: true, min: new Date(1920, 1, 1), max: new Date(2012, 1, 1) },
     isLGBTQIA: { type: Boolean, default: false },  
-    
     gender: { 
         type: String, 
         enum: ['male', 'female', 'non-binary', 'other'], 
         default: 'other'  
     },
-    
-    isAdmin:{ type: Boolean, default: false }
+    isAdmin: { type: Boolean, default: false }
+}, {
+    toJSON: {
+        transform: function(doc, ret) {
+            if (ret.birthDate) {
+                ret.birthDate = ret.birthDate.toISOString().split('T')[0];
+            }
+            return ret;
+        }
+    }
 });
 
 module.exports = mongoose.model('users', usersSchema);
