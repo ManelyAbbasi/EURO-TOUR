2fa176fa3498a129737b3e14b14f06115952c839
labbaci <labbaci@student.chalmers.se>
2024-09-29 16:17:08 +0200

    #23 Implement carousel and fix panels

--- client/src/views/MainCities.vue
+++ client/src/views/MainCities.vue
@@ -27,6 +27,64 @@
         <div class="layout-wrapper">
             <div class="right-side-panel">
                 <!--carousel of cities-->
+                <p>hello</p>
+                <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+          </b-carousel-slide>
+        </b-carousel>
+
+        <p class="mt-4">
+        Slide #: {{ slide }}<br>
+        Sliding: {{ sliding }}
+        </p>
+    </div>
             </div>
             <div class="left-side-panel">
                 <!--search and trending-->
@@ -45,7 +103,22 @@
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -131,7 +204,7 @@
 
 .layout-wrapper {
   display: grid;
-  grid-template-columns: 1fr 1fr;  /* Create two equal columns */
+  grid-template-columns: 2fr 1fr;  /* Create two equal columns */
   grid-gap: 20px;  /* Optional: Add some space between columns */
   padding: 7rem 9% 2rem;
   width: 100%;
@@ -146,6 +219,11 @@
 .right-side-panel {
   display: flex;
   flex-direction: column;
+  background-color: #edf7fb;
+}
+
+.left-side-panel {
+    background-color: #759cab;
 }
 
 .logo img{
