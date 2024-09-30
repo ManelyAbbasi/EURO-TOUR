98ccc75fea9aa81c8a042e35604e1af1913daedc
renaa <renaa@chalmers.se>
2024-09-30 16:48:33 +0200

    #18 Add tags to citiesModel & placesToVisitModel

--- server/models/placesToVisitModel.js
+++ server/models/placesToVisitModel.js
@@ -2,15 +2,44 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 const ReviewsModel = require('./reviewsModel'); // Assuming you're importing the model, not just the schema
 
-// Define the schema for placesToVisit
 var placesToVisitSchema = new Schema({
     placeName: { type: String, required: true },
     address: { type: String, required: true, unique: true, sparse: true },
     rating: { type: Number, min: 0.0, max: 5.0 },
     content: { type: String, required: true },
-    tags: { type: Array, required: true },
-    city: { type: Schema.Types.ObjectId, ref: 'cities', required: true }, 
-    reviews: [{ type: Schema.Types.ObjectId, ref: 'reviews' }] // Use ObjectId to reference the reviews
+    tags: {
+        type: [String], 
+        required: true,
+        validate: {
+            validator: function(tags) {
+                const allowedTags = [
+                    'historical', 
+                    'adventurous', 
+                    'quiet', 
+                    'party', 
+                    'architecture', 
+                    'sight-seeing', 
+                    'museum', 
+                    'new', 
+                    'foodie', 
+                    'nature', 
+                    'foresty', 
+                    'beachy', 
+                    'hot weather', 
+                    'cold weather', 
+                    'varied weather', 
+                    'popular', 
+                    'cheap', 
+                    'pricey', 
+                    'unique'
+                ];
+                return tags.every(tag => allowedTags.includes(tag));
+            },
+            message: 'Invalid tag/tags provided.'
+        }
+    },
+    city: { type: Schema.Types.ObjectId, ref: 'cities', required: true },
+    reviews: [{ type: Schema.Types.ObjectId, ref: 'reviews' }]
 });
 
 // Export the model based on the schema
