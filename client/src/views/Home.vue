cf1670a561e167ba35644203c3b6588534ccea1e
labbaci <labbaci@student.chalmers.se>
2024-10-01 18:50:10 +0200

    #15 Fix header to show respective navs

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,14 +6,15 @@
       </div>
       <nav class="navbar">
         <a href="#favourites" class="navbar-item" v-if="isLoggedIn"><i class="fa-regular fa-heart" style="color: #edf7fb;"></i> favourites</a>
-        <router-link to="/maincities" class="navbar-item"><i class="fa-solid fa-city" style="color: #edf7fb;"></i> cities</router-link>
-        <a href="#placesToVisit" class="navbar-item"><i class="fa-solid fa-map-pin" style="color: #edf7fb;"></i> places to visit</a>
+        <router-link to="/maincities" class="navbar-item" v-if="isLoggedIn"><i class="fa-solid fa-city" style="color: #edf7fb;"></i> cities</router-link>
+        <a href="#placesToVisit" class="navbar-item" v-if="isLoggedIn"><i class="fa-solid fa-map-pin" style="color: #edf7fb;"></i> places to visit</a>
         <b-dropdown
           size="lg"
           variant="link"
           toggle-class="text-decoration-none"
           no-caret
           class="navbar-item dropdown"
+          v-if="!isLoggedIn"
         >
           <template #button-content>
             <img src="@/assets/sign-in-icon.png" alt="Sign In" class="dropdown-icon" />
@@ -21,6 +22,21 @@
           <!-- Dropdown items -->
           <b-dropdown-item class="dropdown-item" to="/login">Log in</b-dropdown-item>
           <b-dropdown-item class="dropdown-item" to="/signup">Sign up</b-dropdown-item>
+        </b-dropdown>
+        <b-dropdown
+          size="lg"
+          variant="link"
+          toggle-class="text-decoration-none"
+          no-caret
+          class="navbar-item dropdown"
+          v-if="isLoggedIn"
+        >
+          <template #button-content>
+            <img src="@/assets/signed-in-icon.png" alt="Sign In" class="dropdown-icon" />
+          </template>
+          <!-- Dropdown items -->
+          <b-dropdown-item class="dropdown-item" to="/">Log out</b-dropdown-item>
+          <b-dropdown-item class="dropdown-item" to="/profile">Profile</b-dropdown-item>
         </b-dropdown>
       </nav>
     </header>
@@ -211,7 +227,8 @@ export default {
 
 .navbar a:hover,
 .logo-wrapper img,
-.navbar a:hover .fa-solid {
+.navbar a:hover .fa-solid,
+.navbar a:hover .fa-regular {
   color: #bc672a !important;
 }
 
