f67638a202536293bf675c13c2c8ed1418c7016b
manely <manely@student.chalmers.se>
2024-10-01 14:45:53 +0200

    #24 Add searchCityByTag to router.js

--- client/src/router.js
+++ client/src/router.js
@@ -3,15 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Map from './views/Map.vue'
 import SignUp from './views/SignUp.vue'
-import MainCities from './views/MainCities.vue'
 import Login from './views/Login.vue'
+import MainCities from './views/MainCities.vue'
+import searchCityByTag from './views/searchCityByTag.vue'
+
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/signup', name: 'signup', component: SignUp },
   { path: '/map', name: 'map', component: Map },
   { path: '/maincities', name: 'maincities', component: MainCities },
-  { path: '/login', name: 'login', component: Login }
+  { path: '/login', name: 'login', component: Login },
+  { path: '/searchCityByTag', name: 'citytag', component: searchCityByTag }
 ]
 
 const router = createRouter({
