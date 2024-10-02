d7bec4c4de73cc61be5dfaf3925c4e9fa26fee9a
renaa <renaa@chalmers.se>
2024-10-02 12:41:30 +0200

    #29 Delete Reviews from userController and route

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -15,12 +15,8 @@ router.delete('/:username', usersControllers.deleteOneUser);
 
 router.delete('/user/:username', usersControllers.deleteUserByAdmin); 
 
-router.get('/:username/reviews', usersControllers.getUserReviews);
-
 router.delete('/placesToVisit/:address', usersControllers.deletePlaceViaAdmin);
 
 router.delete('/cities/:id', usersControllers.deleteCityViaAdmin);
-
-router.delete('/:username/reviews/:review_id', usersControllers.deleteReviewViaAdmin);
 
 module.exports = router;
