ab97baec23392a9baefd167d8d1c3a83dc2c618e
renaa <renaa@chalmers.se>
2024-10-08 15:08:06 +0200

    #39 Add weather warning map for logged in users

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
-import Map from './views/Map.vue'
 import SignUp from './views/SignUp.vue'
 import Login from './views/Login.vue'
 import MainCities from './views/MainCities.vue'
@@ -11,7 +10,6 @@ import Profile from './views/Profile.vue'
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/signup', name: 'signup', component: SignUp },
-  { path: '/map', name: 'map', component: Map },
   { path: '/maincities', name: 'maincities', component: MainCities },
   { path: '/login', name: 'login', component: Login },
   { path: '/searchCityByTag', name: 'citytag', component: searchCityByTag },
