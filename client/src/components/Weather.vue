0e18208ba95b0aefb485a72d077e93ac728067af
manely <manely@student.chalmers.se>
2024-10-14 21:39:50 +0200

    #57 Expand weather map

--- client/src/components/Weather.vue
+++ client/src/components/Weather.vue
@@ -20,13 +20,14 @@ export default {
 .map-container {
   position: relative;
   overflow: hidden;
-  height: 45vh;
+  height: 40vh;
 }
 
 .meteoalarm-map {
   width: 100%;
-  height: calc(45vh + 60px);
+  height: calc(103vh + 60px);
   border: none;
-  margin-top: -60px;
+  margin-top: -80px;
+  border-radius: 1.5rem;
 }
 </style>
